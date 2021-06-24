75761480f7ded9f0f2e8b613e24ff637b96e737d
PraveennKumar <praveensinghh0528@gmail.com>
2021-06-24 12:27:41 +0530

    form updated

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Form>
+      <template class="head" v-slot:head>
+        <h1>Vue js projects</h1>
+        <h2>Form</h2>
+      </template>
+      <template class="main" v-slot:main>
+        <form v-on:submit="onSubmit">
+          <p>Name:</p>
+          <input
+            type="text"
+            placeholder="name"
+            name="name"
+            id="naam"
+            v-model="name"
+          /><br />
+          <br />
+          <p>Age:</p>
+          <input
+            type="number"
+            placeholder="age"
+            id="Umar"
+            v-model="age"
+          /><br />
+          <br />
+          <button type="Submit">Submit</button>
+          <p v-if="msg">{{ msg }}</p>
+        </form>
+      </template>
+      <template v-slot:footer>
+        <h3>Copyright 2021-Praveen</h3>
+      </template>
+    </Form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Form,
+  },
+  data() {
+    return {
+      name: "",
+      age: "",
+      msg: "",
+    };
+  },
+  methods: {
+    onSubmit: function (e) {
+      e.preventDefault();
+      console.log(this.name, this.age);
+      if (!this.name || !this.age) {
+        this.msg = "user name and  age are compulsory to filled";
+      } else {
+        this.msg = "Form submitted successfully";
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  color: white;
+  background-color: grey;
+  width: 250px;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <slot name="head">head</slot>
+  <slot name="main"> </slot>
+  <slot name="footer"> </slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+<style>
+
+</style>
